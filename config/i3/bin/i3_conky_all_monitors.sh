bfd1433461a8aa529e9f736455c9e4a24a80ed29
JordanWu <jordankhwu@gmail.com>
2021-05-27 07:52:09 +0800

    update

--- config/i3/bin/i3_conky_all_monitors.sh
+++ config/i3/bin/i3_conky_all_monitors.sh
@@ -3,11 +3,11 @@
 killall conky
 
 xrandr --output eDP1 --auto --primary --left-of HDMI1
-i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
 i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_hotkey'
+i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
 
-sleep 3
+sleep 2
 
 xrandr --output HDMI1 --auto --primary --right-of eDP1
-i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
 i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_hotkey'
+i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
--- config/i3/bin/i3_conky_eDP1.sh
+++ config/i3/bin/i3_conky_eDP1.sh
@@ -3,9 +3,9 @@
 killall conky
 
 xrandr --output eDP1 --auto --primary --left-of HDMI1
-i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
 i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_hotkey'
+i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
 
-sleep 5
+sleep 2
 
 xrandr --output HDMI1 --auto --primary --right-of eDP1

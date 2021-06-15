70ae31f7f777a16f73be0b7efce327165c86d242
JordanWu <jordankhwu@gmail.com>
2021-06-16 04:53:21 +0800

    update_workspace_management

---
+++ config/i3/bin/i3_restore_all_workspace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+i3-resurrect restore -w 1:E1
+i3-resurrect restore -w 2:E2
+i3-resurrect restore -w 3:E3
+i3-resurrect restore -w 4:E4
+i3-resurrect restore -w 5:E5
+i3-resurrect restore -w 6:E6
+i3-resurrect restore -w 7:E7
+i3-resurrect restore -w 8:E8
+i3-resurrect restore -w 9:E9
+i3-resurrect restore -w 10:E10
+i3-resurrect restore -w 11:H1
+i3-resurrect restore -w 12:H2
+i3-resurrect restore -w 13:H3
+i3-resurrect restore -w 14:H4
+i3-resurrect restore -w 15:H5
+i3-resurrect restore -w 16:H6
+i3-resurrect restore -w 17:H7
+i3-resurrect restore -w 18:H8
+i3-resurrect restore -w 19:H9
+i3-resurrect restore -w 20:H10
+i3-resurrect restore -w 21:H11
+i3-resurrect restore -w 22:H12
---
+++ config/i3/bin/i3_save_all_workspace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+i3-resurrect save -w 1:E1
+i3-resurrect save -w 2:E2
+i3-resurrect save -w 3:E3
+i3-resurrect save -w 4:E4
+i3-resurrect save -w 5:E5
+i3-resurrect save -w 6:E6
+i3-resurrect save -w 7:E7
+i3-resurrect save -w 8:E8
+i3-resurrect save -w 9:E9
+i3-resurrect save -w 10:E10
+i3-resurrect save -w 11:H1
+i3-resurrect save -w 12:H2
+i3-resurrect save -w 13:H3
+i3-resurrect save -w 14:H4
+i3-resurrect save -w 15:H5
+i3-resurrect save -w 16:H6
+i3-resurrect save -w 17:H7
+i3-resurrect save -w 18:H8
+i3-resurrect save -w 19:H9
+i3-resurrect save -w 20:H10
+i3-resurrect save -w 21:H11
+i3-resurrect save -w 22:H12

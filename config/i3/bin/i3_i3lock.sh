6adbf1286a68c43d2a8f16ea9cc6eee04954c62c
JordanWu <jordankhwu@gmail.com>
2021-06-23 01:27:30 +0800

    add_spt_and_lock_png

--- config/i3/bin/i3_i3lock.sh
+++ config/i3/bin/i3_i3lock.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-if ( file $HOME/.config/i3/share/default_wallpaper | grep -q PNG ); then
-    i3lock -t -f -i $HOME/.config/i3/share/default_wallpaper
+current="$HOME/.config/i3/share/default_wallpaper"
+default="$HOME/.config/i3/share/default_thinkpad_wallpaper.png"
+
+if ( file $current | grep -q PNG ); then
+    if [ -f $current.png ]; then
+        rm -f $current.png
+    fi
+    i3lock -t -f -i $current
 else
-    i3lock -t -f -i $HOME/.config/i3/share/default_thinkpad_wallpaper.png
+    if [ $1 == "default" ]; then
+        i3lock -t -f -i $default
+    elif [ $1 == "current" ]; then
+        if [ ! -f $current.png ]; then
+            mogrify -format png $current
+        fi
+        i3lock -t -f -i $current.png
+    else
+        i3lock -t -f -i $default
+    fi
 fi
--- config/i3/bin/i3_xsslock.sh
+++ config/i3/bin/i3_xsslock.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+current="$HOME/.config/i3/share/default_wallpaper"
+default="$HOME/.config/i3/share/default_thinkpad_wallpaper.png"
+
 killall xss-lock
 
-if ( file $HOME/.config/i3/share/default_wallpaper | grep -q PNG ); then
-    xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $HOME/.config/i3/share/default_wallpaper
+if ( file $current | grep -q PNG ); then
+    if [ -f $current.png ]; then
+        rm -f $current.png
+    fi
+    xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $current
 else
-    xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $HOME/.config/i3/share/default_thinkpad_wallpaper.png
+    if [ $1 == "default" ]; then
+        xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $default
+    elif [ $1 == "current" ]; then
+        if [ ! -f $current.png ]; then
+            mogrify -format png $current
+        fi
+        xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $current.png
+    else
+        xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $default
+    fi
 fi

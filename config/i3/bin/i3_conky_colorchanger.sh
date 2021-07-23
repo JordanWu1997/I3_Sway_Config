7f1188f043c5280ec4a050a431a8a568628f9597
JordanWu <jordankhwu@gmail.com>
2021-07-23 23:16:24 +0800

    add_title_color_to_conky

--- config/i3/bin/i3_conky_colorchanger.sh
+++ config/i3/bin/i3_conky_colorchanger.sh
@@ -1,52 +1,73 @@
 #!/usr/bin/bash
 
 # Default color (color1), primary color (color2) in conky configuration
-col_color1=32
-col_color2=33
+if [ $1 == "system" ]; then
+    col_color_text=$(awk '$0~/default_color/ {print NR}' $HOME/.config/conky/conky_config_system | awk 'NR==1')
+    col_color_item=$(awk '$0~/color2/ {print NR}' $HOME/.config/conky/conky_config_system | awk 'NR==1')
+    col_color_title=$(awk '$0~/color3/ {print NR}' $HOME/.config/conky/conky_config_system | awk 'NR==1')
+elif [ $1 == "hotkey" ]; then
+    col_color_text=$(awk '$0~/default_color/ {print NR}' $HOME/.config/conky/conky_config_hotkey | awk 'NR==1')
+    col_color_item=$(awk '$0~/color2/ {print NR}' $HOME/.config/conky/conky_config_hotkey | awk 'NR==1')
+    col_color_title=$(awk '$0~/color3/ {print NR}' $HOME/.config/conky/conky_config_hotkey | awk 'NR==1')
+fi
 
-# Read font color from wal Xresources
-color1="$(awk '$1~/*color15/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+# Read text color from wal Xresources
+color_text="$(awk '$1~/*color15/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
 
-# Read font color (content) from wal Xresources based on input
-input_color=$2
+# Read item color (content) from wal Xresources based on input
+input_color=$3
 case $input_color in
     color0)
-        color2="$(awk '$1~/*color0:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
-        echo $color2
+        output_color="$(awk '$1~/*color0:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color1)
-        color2="$(awk '$1~/*color1:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
-        echo $color2
+        output_color="$(awk '$1~/*color1:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color2)
-        color2="$(awk '$1~/*color2:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+        output_color="$(awk '$1~/*color2:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color3)
-        color2="$(awk '$1~/*color3:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+        output_color="$(awk '$1~/*color3:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color4)
-        color2="$(awk '$1~/*color4:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+        output_color="$(awk '$1~/*color4:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color5)
-        color2="$(awk '$1~/*color5:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+        output_color="$(awk '$1~/*color5:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color6)
-        color2="$(awk '$1~/*color6:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+        output_color="$(awk '$1~/*color6:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
     color7)
-        color2="$(awk '$1~/*color7:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
+        output_color="$(awk '$1~/*color7:/ {print substr($2,2,7)}' $HOME/.cache/wal/colors.Xresources)"
         ;;
 esac
 
 # Replace color in conky configuration file
 if [ "$1" == "system" ]; then
     # Conky for system
-    sed -i "$col_color1 s/.*/\tdefault_color \= \'\#$color1\\',/" "$HOME/.config/conky/conky_config_system"
-    sed -i "$col_color2 s/.*/\tcolor2 \= \'\#$color2\\',/" "$HOME/.config/conky/conky_config_system"
+    sed -i "$col_color_text s/.*/\tdefault_color \= \'\#$color_text\\',/" "$HOME/.config/conky/conky_config_system"
+    if [ "$2" == "item" ]; then
+        sed -i "$col_color_item s/.*/\tcolor2 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_system"
+    elif [ "$2" == "title" ]; then
+        sed -i "$col_color_title s/.*/\tcolor3 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_system"
+    # Same for both
+    else
+        sed -i "$col_color_item s/.*/\tcolor2 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_system"
+        sed -i "$col_color_title s/.*/\tcolor3 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_system"
+    fi
 elif [ "$1" == "hotkey" ]; then
     # Conky for hotkey
-    sed -i "$col_color1 s/.*/\tdefault_color \= \'\#$color1\\',/" "$HOME/.config/conky/conky_config_hotkey"
-    sed -i "$col_color2 s/.*/\tcolor2 \= \'\#$color2\\',/" "$HOME/.config/conky/conky_config_hotkey"
+    sed -i "$col_color_text s/.*/\tdefault_color \= \'\#$color_text\\',/" "$HOME/.config/conky/conky_config_hotkey"
+    if [ "$2" == "item" ]; then
+        sed -i "$col_color_item s/.*/\tcolor2 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_hotkey"
+    elif [ "$2" == "title" ]; then
+        sed -i "$col_color_title s/.*/\tcolor3 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_hotkey"
+    # Same for both
+    else
+        sed -i "$col_color_item s/.*/\tcolor2 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_hotkey"
+        sed -i "$col_color_title s/.*/\tcolor3 \= \'\#$output_color\\',/" "$HOME/.config/conky/conky_config_hotkey"
+    fi
 else
     # Wrong input, send error message
     echo "Wrong input, color not change. Try system or rofi as input"

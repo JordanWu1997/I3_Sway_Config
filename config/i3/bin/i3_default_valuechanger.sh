1090d990daad97031bbe2be814e7fc189f246df1
JordanWu <jordankhwu@gmail.com>
2021-08-24 13:03:52 +0800

    update_titlebar_fontsize_mode

--- config/i3/bin/i3_default_valuechanger.sh
+++ config/i3/bin/i3_default_valuechanger.sh
@@ -9,16 +9,23 @@ I3_CONFIG_FILE="$HOME/.config/i3/config"
 COL_OUTER_GAP_WIDTH=$(awk '$0~/default_outer_gap/ {print NR}' $HOME/.config/i3/config | awk 'NR==1')
 COL_INNER_GAP_WIDTH=$(awk '$0~/default_inner_gap/ {print NR}' $HOME/.config/i3/config | awk 'NR==1')
 COL_BORDER_WIDTH=$(awk '$0~/default_border_width/ {print NR}' $HOME/.config/i3/config | awk 'NR==1')
+COL_TITLEBAR_FONTSIZE=$(awk '$0~/default_titlebar_fontsize/ {print NR}' $HOME/.config/i3/config | awk 'NR==1')
 
 # Set new default value in i3 configuration file
 if [ $CHANGE_ITEM == "outer_gap" ]; then
     sed -i "$COL_OUTER_GAP_WIDTH s/.*/set \$default_outer_gap $NEW_DEFAULT_VALUE/" $I3_CONFIG_FILE
+    i3-msg reload
 elif [ $CHANGE_ITEM == "inner_gap" ]; then
     sed -i "$COL_INNER_GAP_WIDTH s/.*/set \$default_inner_gap $NEW_DEFAULT_VALUE/" $I3_CONFIG_FILE
+    i3-msg reload
 elif [ $CHANGE_ITEM == "border_width" ]; then
     sed -i "$COL_BORDER_WIDTH s/.*/set \$default_border_width pixel $NEW_DEFAULT_VALUE/" $I3_CONFIG_FILE
+    i3-msg reload
+elif [ $CHANGE_ITEM == "titlebar_fontsize" ]; then
+    sed -i "$COL_TITLEBAR_FONTSIZE s/.*/set \$default_titlebar_fontsize $NEW_DEFAULT_VALUE/" $I3_CONFIG_FILE
+    i3-msg reload
 else
     echo ""
-    echo "Wrong input [Available option: outer_gap/inner_gap/border_width]"
+    echo "Wrong input [Available option: outer_gap/inner_gap/border_width/titlebar_fontsize]"
     echo ""
 fi

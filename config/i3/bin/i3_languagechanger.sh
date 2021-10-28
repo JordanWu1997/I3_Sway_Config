15ef0dc11eaca34edd51a18198d25ee3ab189c2d
JordanWu <jordankhwu@gmail.com>
2021-10-29 01:31:49 +0800

    update_ibus

---
+++ config/i3/bin/i3_languagechanger.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Source code
+# -- https://www.reddit.com/r/i3wm/comments/jct4ti/i3wm_gaps_cant_switch_ibus_method/
+
+EN_ibus="xkb:us::eng"
+CH_ibus="chewing"
+lang=`ibus engine`
+
+if [ $lang = $EN_ibus ]; then
+    notify-send "[ibus-CH]: Chinese (chewing)"
+    ibus engine $CH_ibus
+fi
+
+if [ $lang = $CH_ibus ]; then
+    notify-send "[ibus-EN]: English (xkb:us::eng)"
+    ibus engine $EN_ibus
+fi

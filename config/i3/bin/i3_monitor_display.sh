65d0a4d84fe78d0ab7e8d7418f87dc17cdc3c0ff
JordanWu <jordankhwu@gmail.com>
2021-11-04 10:24:13 +0800

    fix_monitor_display_script_xrandr_issue

--- config/i3/bin/i3_monitor_display.sh
+++ config/i3/bin/i3_monitor_display.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
 # Get HDMI1 Parameter
+# Note: xrandr HDMI1 display information is added when HDMI1 is plugged
 HDMI_POS=$1
 HDMI1_STATUS=$(xrandr | awk '$1~/HDMI1/ {print $2}')
-HDMI1_WIDTH=$(xrandr | awk '$1~/HDMI1/ {print $13}')
-HDMI1_HEIGHT=$(xrandr | awk '$1~/HDMI1/ {print $15}')
+HDMI1_STATUS_LEN=$(xrandr | awk '$1~/HDMI1/ {print NF}')
+HDMI1_HEIGHT_ID=$HDMI1_STATUS_LEN
+HDMI1_WIDTH_ID=$(($HDMI1_HEIGHT_ID - 2))
+HDMI1_HEIGHT=$(xrandr | awk -v var=$HDMI1_HEIGHT_ID '$1~/HDMI1/ {print $var}')
+HDMI1_WIDTH=$(xrandr | awk -v var=$HDMI1_WIDTH_ID '$1~/HDMI1/ {print $var}')
+echo HDMI1 HxW: $HDMI1_HEIGHT, $HDMI1_WIDTH
 
 # Joint Display
 if [ $HDMI1_STATUS == 'connected' ]; then
     if [ $HDMI_POS == 'right' ]; then
 
-        # ACER 27': 600mm x 340mm
-        if [ $HDMI1_WIDTH == "600mm" ] && [ $HDMI1_HEIGHT == "340mm" ]; then
-            echo "ACER 27'"
-            # Locate eDP1 & HDMI1
-            $I3_BIN/i3_shrink_eDP1.sh && xrandr \
-                --output eDP1 --pos 0x270 --output HDMI1 --pos 1440x0 \
-                --output eDP1 --mode 1440x810_60.00 \
-                --output HDMI1 --mode 1920x1080 --primary
-
         # IOA 24': 520mm x 290mm
-        elif [ $HDMI1_WIDTH == "520mm" ] && [ $HDMI1_HEIGHT == "290mm" ]; then
+        if [ $HDMI1_WIDTH == "520mm" ] && [ $HDMI1_HEIGHT == "290mm" ]; then
             echo "IOA 24'"
             # Locate eDP1 & HDMI1
             $I3_BIN/i3_extend_HDMI1.sh && $I3_BIN/i3_shrink_eDP1.sh && xrandr \
                 --output eDP1 --pos 0x195 --output HDMI1 --pos 1440x0 \
                 --output eDP1 --mode 1440x810_60.00 \
                 --output HDMI1 --mode 1920x1200_50.00 --primary
+
+        # ACER 27': 600mm x 340mm
+        elif [ $HDMI1_WIDTH == "600mm" ] && [ $HDMI1_HEIGHT == "340mm" ]; then
+            echo "ACER 27'"
+            # Locate eDP1 & HDMI1
+            $I3_BIN/i3_shrink_eDP1.sh && xrandr \
+                --output eDP1 --pos 0x270 --output HDMI1 --pos 1440x0 \
+                --output eDP1 --mode 1440x810_60.00 \
+                --output HDMI1 --mode 1920x1080 --primary
 
         # Other HDMI:
         else
@@ -41,9 +46,9 @@ else
     xrandr --output eDP1 --mode 1920x1080 --primary --output HDMI1 --off
 fi
 
-# Reload wallpaper
+## Reload wallpaper
 feh --bg-fill $HOME/.config/i3/share/default_wallpaper
 
-# Reload compositor
-/usr/local/bin/picom --config $HOME/.config/picom/picom_blur.conf
-#/usr/local/bin/picom --config $HOME/.config/picom/picom_transparency.conf
+ #Reload compositor
+killall picom
+picom --config /home/jordankhwu/.config/picom/picom.conf

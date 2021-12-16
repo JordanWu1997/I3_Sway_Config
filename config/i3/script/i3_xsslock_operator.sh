f8dd4770867ca51109ea40ab0da46ebe29918468
JordanWu <jordankhwu@gmail.com>
2021-12-17 06:51:01 +0800

    update

--- config/i3/script/i3_xsslock_operator.sh
+++ config/i3/script/i3_xsslock_operator.sh
@@ -9,14 +9,15 @@ if ( file $current | grep -q PNG ); then
     if [ -f $current.png ]; then
         rm -f $current.png
     fi
-    xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $current
+    # Note: i3lock -n flag is needed for unlock functionality
+    xss-lock -v --transfer-sleep-lock -- i3lock -nfti $current
 else
     if [ $1 == "default" ]; then
-        xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $default
+        xss-lock -v --transfer-sleep-lock -- i3lock -nfti $default
     elif [ $1 == "current" ]; then
         mogrify -resize 1920x1080 -format png $current; sleep 1
-        xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $current.png
+        xss-lock -v --transfer-sleep-lock -- i3lock -nfti $current.png
     else
-        xss-lock -v --transfer-sleep-lock -- i3lock -t -f -i $default
+        xss-lock -v --transfer-sleep-lock -- i3lock -nfti $default
     fi
 fi

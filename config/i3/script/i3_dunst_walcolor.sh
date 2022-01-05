9a8558d168cae2dc7ffcad9dcb93600cff2ba8ee
JordanWu <jordankhwu@gmail.com>
2022-01-05 16:52:42 +0800

    update_dunst

--- config/i3/script/i3_dunst_walcolor.sh
+++ config/i3/script/i3_dunst_walcolor.sh
@@ -15,23 +15,27 @@
 pidof dunst && killall dunst
 
 # Template for monochrome color style
-dunst -lf  "${color7:-#ffffff}" \
-      -lb  "${color1:-#000000}" \
-      -lfr "${color4:-#2e9ef4}" \
-      -nf  "${color7:-#ffffff}" \
-      -nb  "${color1:-#000000}" \
-      -nfr "${color4:-#2e9ef4}" \
-      -cf  "${color7:-#ffffff}" \
-      -cb  "${color1:-#000000}" \
-      -cfr "${color4:-#2e9ef4}" > /dev/null 2>&1 &
+dunst \
+    -lf  "${color7:-#ffffff}" \
+    -lb  "${color1:-#000000}" \
+    -lfr "${color4:-#2e9ef4}" \
+    -nf  "${color7:-#ffffff}" \
+    -nb  "${color1:-#000000}" \
+    -nfr "${color4:-#2e9ef4}" \
+    -cf  "${color7:-#ffffff}" \
+    -cb  "${color1:-#000000}" \
+    -cfr "${color4:-#2e9ef4}" \
+    > /dev/null 2>&1 &
 
 ## Template for non-monochrome color style
-#dunst -lf  "${color0:-#ffffff}" \
-      #-lb  "${color1:-#000000}" \
-      #-lfr "${color4:-#2e9ef4}" \
-      #-nf  "${color0:-#ffffff}" \
-      #-nb  "${color1:-#000000}" \
-      #-nfr "${color4:-#2e9ef4}" \
-      #-cf  "${color0:-#ffffff}" \
-      #-cb  "${color1:-#000000}" \
-      #-cfr "${color4:-#2e9ef4}" > /dev/null 2>&1 &
+#dunst \
+    #-lf  "${color0:-#ffffff}" \
+    #-lb  "${color1:-#000000}" \
+    #-lfr "${color4:-#2e9ef4}" \
+    #-nf  "${color0:-#ffffff}" \
+    #-nb  "${color1:-#000000}" \
+    #-nfr "${color4:-#2e9ef4}" \
+    #-cf  "${color0:-#ffffff}" \
+    #-cb  "${color1:-#000000}" \
+    #-cfr "${color4:-#2e9ef4}" \
+    #> /dev/null 2>&1 &

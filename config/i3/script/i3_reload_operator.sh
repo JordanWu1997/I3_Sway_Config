8b068ff4c12d8b751c61a50c41e77e08fc728618
JordanWu <jordankhwu@gmail.com>
2022-01-27 00:08:56 +0800

    update

--- config/i3/script/i3_reload_operator.sh
+++ config/i3/script/i3_reload_operator.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Conky (system monitor & shortcut table)
-i3_conky_colorchanger.sh system all; sleep 1
-i3_conky_colorchanger.sh bindkey all; sleep 1
+$I3_SCRIPT/i3_conky_colorchanger.sh system all; sleep 1
+$I3_SCRIPT/i3_conky_colorchanger.sh bindkey all; sleep 1
 notify-send -u low "Theme Mode" "Reload conky"
 
 # Dunst (notifier)
-i3_dunst_walcolor.sh both
+$I3_SCRIPT/i3_dunst_walcolor.sh both
 notify-send -u low "Theme Mode" "Reload dunst (notifier)"
 
 # Xsslock (lock for xwindow)
-i3_xsslock_operator.sh current
+$I3_SCRIPT/i3_xsslock_operator.sh current
 notify-send -u low "Theme Mode" "Reload xsslock (session lock)"
 
 # Vis (visualizer)
-i3_vis_colorchanger.sh
+$I3_SCRIPT/i3_vis_colorchanger.sh
 notify-send -u low "Theme Mode" "Reload vis (cli-equalizer)"
 
 # Zathura (PDF viewer)

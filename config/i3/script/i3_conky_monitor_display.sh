7348679489318ba9ff0809f338f47aad48fc37e6
JordanWu <jordankhwu@gmail.com>
2022-02-17 23:04:50 +0800

    update_spotifyd_dunst

--- config/i3/script/i3_conky_monitor_display.sh
+++ config/i3/script/i3_conky_monitor_display.sh
@@ -32,6 +32,10 @@ case $1 in
         xrandr --output HDMI1 --primary --right-of eDP1
         i3-msg restart
         ;;
+    "default")
+        i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_bindkey'
+        i3-msg 'exec --no-startup-id conky -c ~/.config/conky/conky_config_system'
+        ;;
     *)
         echo "Availble option: all/primary/primary_only/eDP1"
 esac

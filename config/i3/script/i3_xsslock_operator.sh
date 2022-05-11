e332a2fe464ab48c44fb81e3e204bc230f48043d
JordanWu <jordankhwu@gmail.com>
2022-05-11 09:44:00 +0800

    update_xsslock

--- config/i3/script/i3_xsslock_operator.sh
+++ config/i3/script/i3_xsslock_operator.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-CURRENT="$HOME/.config/i3/share/default_wallpaper"
 DEFAULT="$HOME/.config/i3/share/default_thinkpad_wallpaper.png"
+CURRENT_DEFAULT="$HOME/.config/i3/share/default_wallpaper"
+CURRENT=$(head -n 2 "$HOME/.fehbg" | tail -n 1 | cut -d"'" -f2)
+IMAGESIZE="1920x1080"
 
 killall xss-lock
 
-if ( file $CURRENT | grep -q PNG ); then
-    [ -f $CURRENT.png ] && rm -f $CURRENT.png
-    # Note: i3lock -n flag is needed for unlock functionality
-    i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $CURRENT"
-else
-    case $1 in
-        "default")
-            i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $DEFAULT"
-            ;;
-        "current")
-            mogrify -resize 1920x1080 -format png $CURRENT
-            sleep 1
-            i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $CURRENT.png"
-            ;;
-        *)
-            i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $DEFAULT"
-            ;;
-    esac
-fi
+[ -f $CURRENT_DEFAULT.png ] && rm -f $CURRENT_DEFAULT.png
+case $1 in
+    "current_default")
+        mogrify -resize $IMAGESIZE -format png $CURRENT_DEFAULT
+        sleep 1
+        i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $CURRENT_DEFAULT.png"
+        ;;
+    "current")
+        mogrify -resize $IMAGESIZE -format png $CURRENT
+        CURRENT_PNG="$(dirname $CURRENT)/$(basename $CURRENT | cut -d'.' -f1).png"
+        sleep 1
+        i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $CURRENT_PNG"
+        ;;
+    *)
+        i3-msg exec "xss-lock -v --transfer-sleep-lock -- i3lock -nfti $DEFAULT"
+        ;;
+esac

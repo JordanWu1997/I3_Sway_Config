a324a86e0add5ee96d22bbace5895324fa060490
JordanWu <jordankhwu@gmail.com>
2024-09-26 23:25:48 +0800

    update:float_and_fulfill_workspace

--- config/i3/script/i3_window_operator.sh
+++ config/i3/script/i3_window_operator.sh
@@ -12,7 +12,7 @@ show_help_message () {
     echo "  i3_window_operator.sh [operations]"
     echo ""
     echo "OPERATIONS"
-    echo "  [float_then_fullscreen]: make current window floating and resize to monitor size"
+    echo "  [float_then_fullfil_workspace]: make current window floating and resize to fullfil the workspace"
     echo "  [center_current]: make current window floating and move to center of monitor"
     echo "  [toggle_sticky_current]: toggle current window stickiness"
     echo "  [resize_to_input]: resize window to input width and height"
@@ -127,19 +127,26 @@ move_floating_to_input () {
 window_operation () {
     ICON="$HOME/.config/i3/share/64x64/window.png"
     case $1 in
-        "float_then_fullscreen")
+        "float_then_fullfil_workspace")
+            # Get focus window ID
+            CURRENT_WINDOW_ID=$(xdotool getwindowfocus)
             # Get workspace width and height
             WIDTH=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).rect.width')
             HEIGHT=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).rect.height')
             # Get window border width
             I3_CONFIG="$HOME/.config/i3/config"
             BORDER_WIDTH=$(awk '$0~/default_border_width/ {print $3}' ${I3_CONFIG})
+            # Open an empty window to occupy the space to keep the tiling layout in tact
+            CURRENT_FLOATING_STATUS=$(i3-msg -t get_tree | tr \} '\n' | grep ${CURRENT_WINDOW_ID} -A13 | tr \, '\n' | grep '"floating":' | grep 'user_' | cut -d: -f2)
+            if [[ ! ${CURRENT_FLOATING_STATUS} == '"user_on"' ]]; then
+                i3-msg open
+            fi
             # Floating
-            i3-msg "floating enable"
-            i3-msg "border pixel ${BORDER_WIDTH}"
-            # Fullscreen
-            i3-msg "resize set width ${WIDTH} px height ${HEIGHT} px"
-            i3-msg "move position center"
+            i3-msg "[id=${CURRENT_WINDOW_ID}] floating enable"
+            i3-msg "[id=${CURRENT_WINDOW_ID}] border pixel ${BORDER_WIDTH}"
+            # Resize to fulfil lfullscreen
+            i3-msg "[id=${CURRENT_WINDOW_ID}] resize set width ${WIDTH} px height ${HEIGHT} px"
+            i3-msg "[id=${CURRENT_WINDOW_ID}] move position center, focus"
             ;;
         "center_current")
             # Floating
